34a77f8b7d2614d47dbb63f46bdde9b60292487b
Roman Apunts2 <roman@grupapunts.es>
2026-02-05 07:16:28 +0100

    Added .env for timezone user

--- config/app.php
+++ config/app.php
@@ -70,7 +70,7 @@ return [
     |
     */
 
-    'timezone' => 'Asia/Hong_Kong',
+    'timezone' => env('APP_TIMEZONE', 'Asia/Hong_Kong'),
 
     /*
     |--------------------------------------------------------------------------
